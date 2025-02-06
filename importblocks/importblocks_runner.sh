f8ecf089f3bf0d7df59a4d2c347c4ff7889d90af
MK <17832330+mkchungs@users.noreply.github.com>
2025-02-06 02:11:34 -0800

    update

--- importblocks/importblocks_runner.sh
+++ importblocks/importblocks_runner.sh
@@ -2,22 +2,25 @@
 set -e
 
 CONTAINER_NAME="importblocks_container"
-IMAGE="us-central1-docker.pkg.dev/jam-duna/importblocks/importblocks:latest"
+REGISTRY="us-central1-docker.pkg.dev"
+REPOSITORY="importblocks"
+BINARY="importblocks"
+PROJECT="jam-duna"
+IMAGE="${REGISTRY}/${PROJECT}/${REPOSITORY}/${BINARY}:latest"
 
-echo "Stopping container ${CONTAINER_NAME}..."
-docker stop "${CONTAINER_NAME}" 2>/dev/null || echo "No container ${CONTAINER_NAME} running."
+echo "Stopping container ${CONTAINER_NAME} if it exists..."
+docker stop "${CONTAINER_NAME}" 2>/dev/null || echo "Container ${CONTAINER_NAME} is not running."
 
-echo "Removing container ${CONTAINER_NAME}..."
-docker rm "${CONTAINER_NAME}" 2>/dev/null || echo "No container ${CONTAINER_NAME} found."
+echo "Removing container ${CONTAINER_NAME} if it exists..."
+docker rm "${CONTAINER_NAME}" 2>/dev/null || echo "Container ${CONTAINER_NAME} does not exist."
 
-echo "Pulling image ${IMAGE}..."
+#gcloud auth configure-docker us-central1-docker.pkg.dev might be needed
+echo "Pulling image ${IMAGE} from GAR..."
 docker pull "${IMAGE}"
 
-echo "Starting new container ${CONTAINER_NAME}..."
+echo "Running new container ${CONTAINER_NAME} using image ${IMAGE}..."
 docker run -d --name "${CONTAINER_NAME}" --hostname "${CONTAINER_NAME}" "${IMAGE}"
 
-echo "To execute importblocks, run:"
-echo "docker exec -it ${CONTAINER_NAME} ./importblocks --mode=assurances --http='http://yourimplementation:8099/' --invalidrate=0.31459 --numblocks=50 --statistics=25"
-
-echo "To open an interactive shell, run:"
-echo "docker exec -it ${CONTAINER_NAME} /bin/bash"
+echo "Getting started with:"
+echo  "docker exec -it "${CONTAINER_NAME}" ./${BINARY} --mode=safrole --http='http://imeplementation.jamduna.org:8088' --invalidrate=0.31459 --numblocks=50 --statistics=25"
+docker exec -it "${CONTAINER_NAME}" ./${BINARY} --help
