d5fa58692b3e5e1f0a4a736aa39d19df36f5b883
Sourabh Niyogi <sourabh@colorfulnotion.com>
2025-03-04 20:57:15 +0000

    v0.6.2.9 dataset with fuzzing
    Addresses several issues reported by @jaymansfield:
    * #125 Incorporating new services upon panic - Assurances/3_006
    * #123 Add D to chain specs - assurances/3_002
    * #121 Fix state_snapshots lookup_meta value - assurances/2_007
    
    Also:
    * #112 Adjust Chainspecs to be EC friendly as per advice from Dave Emmett
    * update README

--- chainspecs/configs/config_medium.go
+++ chainspecs/configs/config_medium.go
@@ -15,4 +15,8 @@ const (
 	MaxAuthorizationQueueItems  = 80 // Q: The maximum number of items in the authorizations queue.
 	MaxAuthorizationPoolItems   = 8  // O: The maximum number of items in the authorizations pool.
 	ValidatorCoreRotationPeriod = 4  // R: The rotation period of validator-core assignments, in timeslots.
+	SegmentSize                 = 4104
+	ECPieceSize                 = 12
+	NumECPiecesPerSegment       = 342
+	PreimageExpiryPeriod        = 30
 )
