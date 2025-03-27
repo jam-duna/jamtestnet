44b05de7c7ff11677be0810be88ebdef03078ba8
Nicholas Lee <armin97660@gmail.com>
2025-03-27 04:43:04 -0700

    implement workpackage page

---
+++ explorer_main/src/hooks/workpackage/useFetchWorkpackage/index.ts
@@ -0,0 +1,20 @@
+export async function fetchWorkPackage(hash: string, rpcUrl: string): Promise<any> {
+    const payload = {
+      jsonrpc: "2.0",
+      id: 2,
+      method: "jam.GetWorkPackageByHash",
+      params: [hash],
+    };
+    try {
+      const response = await fetch(rpcUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+      return await response.json();
+    } catch (err) {
+      //console.error("Error fetching service:", err);
+      return null;
+    }
+  }
+  
